9413b543d12c19cbbad05b635ad16d4c8ceecabd
Nicolò Santilio <nicolo.santilio@outlook.com>
2025-06-17 11:49:08 +0200

    Declare robot_state_publisher dependency

--- src/ackermann_robot_navigation/launch/bringup_all.launch.py
+++ src/ackermann_robot_navigation/launch/bringup_all.launch.py
@@ -31,6 +31,18 @@ def generate_launch_description():
                 output='screen'
             ),
             Node(
+                package='joint_state_publisher',
+                executable='joint_state_publisher',
+                name='joint_state_publisher',
+                output='screen'
+            ),
+            Node(
+                package='joint_state_publisher_gui',
+                executable='joint_state_publisher_gui',
+                name='joint_state_publisher_gui',
+                output='screen'
+            ),
+            Node(
                 package='robot_state_publisher',
                 executable='robot_state_publisher',
                 name='robot_state_publisher',
